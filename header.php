e0bbb86ed7361d46ff18410d065c5123b253fbd9
P3ter <contact@p3ter.fr>
2018-09-01 01:39:44 +0200

    Correction url wiki

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 				<input type="checkbox" id="menu">
 				<ul class="menu">
 					<?php $plxShow->staticList($plxShow->getLang('HOME'),'<li class="#static_class #static_status" id="#static_id"><a href="#static_url" title="#static_name">#static_name</a></li>'); ?>
-					<li><a href="https://wiki.pluxml.org" title="Wiki PluXml">Documentation</a></li>
+					<li><a href="https://wiki.pluxml.ovh" title="Wiki PluXml">Documentation</a></li>
 					<?php $plxShow->pageBlog('<li class="#page_class #page_status" id="#page_id"><a href="#page_url" title="#page_name">#page_name</a></li>'); ?>
 				</ul>
 			</div>
