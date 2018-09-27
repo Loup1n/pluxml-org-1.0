9b24dfa4fd0a694da02ee952b1402847e944cbdc
P3ter <contact@p3ter.fr>
2018-09-28 00:43:47 +0200

    Ajout screenshot admin sur la home

--- static-home.php
+++ static-home.php
@@ -3,34 +3,30 @@
 	<main class="main">
 
 		<header class="header">
-			<div class="container">
-				<h1 class="no-margin heading-small">Blog ou CMS à l'Xml</h1>
-				<h2 class="h5 no-margin">Simple et léger <br>Sans base de données <br>Collaboratif <br>Thèmes <br>Plugins</h2>
+			<div class="grid">
+				<div class="col sml-8">
+					<h1 class="no-margin heading-small">Blog ou CMS à l'Xml</h1>
+					<h2 class="h5 no-margin">Simple et léger <br>Sans base de données <br>Collaboratif <br>Thèmes <br>Plugins</h2>
+				</div>
+				<div class="col sml-4 text-left">
+					<img class="" src="<?php $plxShow->template(); ?>/img/plx-home-desktop-mobile-2-300px.jpg" alt="PluXml">
+				</div>
 			</div>
 		</header>
 
 		<div class="container">
-
 			<div class="grid">
-
 				<div class="content col sml-12">
-
 					<article class="article static" id="static-page-<?php echo $plxShow->staticId(); ?>">
-
 						<header>
 							<h2>
 								<?php $plxShow->staticTitle(); ?>
 							</h2>
 						</header>
-
 						<?php $plxShow->staticContent(); ?>
-
 					</article>
-
 				</div>
-
 			</div>
-
 		</div>
 
 	</main>
