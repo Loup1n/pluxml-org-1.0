d1c34293dfcc4ed46a3a875a0287b6ecbad0013d
P3ter <contact@p3ter.fr>
2018-10-09 00:08:06 +0200

    Ajout des CTA Démo, Téléchargement, Changelog sur la homepage

--- static-home.php
+++ static-home.php
@@ -9,8 +9,15 @@
 				<div class="grid">
 					<div class="col sml-12 med-6 text-center">
 						<h1 class="heading-small">Blog ou CMS à l'Xml</h1>
+						<h2 class="h5">Créez un site Web performant en toute simplicité</h2>
 						<img alt="Logo PluXml" src="<?php $plxShow->template(); ?>/img/plx-logo-bleu.png">
-						<h2 class="h5">Simple et léger <br>Sans base de données <br>Collaboratif <br>Thèmes <br>Plugins</h2>
+						<p>
+							<a href="https://telechargements.pluxml.org/download.php" title="PluXml 5.6 - 368 Ko"  class="button blue">Télécharger</a>
+							<a href="https://demo.pluxml.org/" title="Site de démonstration de PluXml" class="button blue">Démo</a>
+						</p>
+						<p>
+							<a href="https://telechargements.pluxml.org/changelog" class="h6">Changelog</a>
+						</p>
 					</div>
 					<div class="col sml-12 med-6">
 						<img class="img-border-1" src="<?php $plxShow->template(); ?>/img/plx-home-laptop-300px.png" alt="PluXml">
