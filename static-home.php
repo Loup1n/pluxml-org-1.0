4a0c69e81d6ed1f0569041e90fd3d9f740011fd2
P3ter <contact@p3ter.fr>
2018-10-09 00:41:19 +0200

    Rédaction de la homepage

--- static-home.php
+++ static-home.php
@@ -29,11 +29,23 @@
 		<section class="section-1">
 			<div class="container">
 				<div class="grid">
-					<div class="col sml-12 med-6 text-center">
-						texte
+					<div class="col sml-12 med-4 text-center">
+						<strong>Aucune base de données</strong>
+						<p>
+							Toutes les données sont stockées dans des fichiers xml. Une simple copie de fichiers permet de déplacer votre site sur une clé usb, ce qui le rend complètement portable.
+						</p>
 					</div>
-					<div class="col sml-12 med-6 text-center">
-						texte
+					<div class="col sml-12 med-4 text-center">
+						<strong>Collaboratif</strong>
+						<p>
+							5 profils avec des niveaux d'autorisations différents. Pour la famille, les amis, une entreprise, PluXml trouvera sa place.
+						</p>
+					</div>
+					<div class="col sml-12 med-4 text-center">
+						<strong>Personnalisable</strong>
+						<p>
+							PluXml s'habille suivant vos envies pour que votre site soit à votre image. Etendez votre PluXml sans aucune ligne de programmation en ajoutant facilement des nouvelles fonctionnalités.
+						</p>
 					</div>
 				</div>
 			</div>
@@ -45,8 +57,21 @@
 					<div class="col sml-12 med-6">
 						<img class="img-border-2" src="<?php $plxShow->template(); ?>/img/plx-home-tablet-smartphone-300px.png" alt="PluXml">
 					</div>
-					<div class="col sml-12 med-6 text-center">
-						texte
+					<div class="col sml-12 med-6">
+						<strong>Principales caractéristiques</strong>
+						<ul class="unstyled-list">
+							<li>Aucune base de données requise</li>
+							<li>Portable sur clé usb</li>
+							<li>Multi-utilisateurs avec des niveaux d'autorisations différents</li>
+							<li>Pages statiques, catégories, gestion des tags, archives</li>
+							<li>Gestion des commentaires</li>
+							<li>Gestionnaire de médias : images, documents</li>
+							<li>Flux Rss des articles, commentaires, tags, catégories</li>
+							<li>Traduit en 11 langues (français, allemand, anglais, espagnol, italien, néerlandais, occitan, polonais, portugais, roumain, russe)</li>
+							<li>Thèmes personnalisables (supporte les thèmes pour appareils mobiles et smartphones: iphone, blackberry, pocket-pc...)</li>
+							<li>Plugins</li>
+							<li>Réécriture d'urls</li>
+						</ul>
 					</div>
 				</div>
 			</div>
@@ -55,8 +80,25 @@
 		<section class="section-1">
 			<div class="container">
 				<div class="grid">
-					<div class="col sml-12 med-6 text-center">
-						texte
+					<div class="col sml-12 med-6">
+						<strong>Pré-requis</strong>
+						<ul class="unstyled-list">
+							<li>Serveur Apache, Nginx ou Lighttpd</li>
+							<li>PHP 5 ou supérieur</li>
+							<li>Librairie GD pour la gestion des images</li>
+							<li>Fonction php d'envoi d'email autorisée (non obligatoire)</li>
+						</ul>
+						<strong>Procédure d'installation</strong>
+						<ul class="unstyled-list">
+							<li>Récuperez l'archive téléchargeable sur cette page et dézippez la à la racine de votre site</li>
+							<li>Connectez-vous à votre site et suivez la procédure d'installation affichée à l'écran</li>
+						</ul>
+						<strong>Mise à jour d'une version existante de PluXml</strong>
+						<ul class="unstyled-list">
+							<li><strong>IMPORTANT:</strong> Sauvegardez le dossier data de votre PluXml</li>
+							<li>Récuperez l'archive téléchargeable sur cette page et dézippez la à la racine de votre site de manière à écraser les fichiers existants</li>
+							<li>Connectez-vous à votre site et suivez la procédure de mise à jour affichée à l'écran</li>
+						</ul>
 					</div>
 					<div class="col sml-12 med-6">
 						<img src="<?php $plxShow->template(); ?>/img/plx-home-admin-desktop-mobile-300px.png" alt="PluXml">
@@ -68,11 +110,39 @@
 		<section class="section-2">
 			<div class="container">
 				<div class="grid">
-					<div class="col sml-12 med-6 text-center">
-						texte
+					<div class="col sml-12 med-3 text-center">
+						<img alt="Logo PluXml" src="<?php $plxShow->template(); ?>/img/plx-logo-bleu.png">
 					</div>
-					<div class="col sml-12 med-6 text-center">
-						texte
+					<div class="col sml-6 med-2">
+						<strong>A propos de PluXml</strong>
+						<ul class="unstyled-list">
+							<li><a title="A propos" href="https://www.pluxml.org/static2/about">A propos</a></li>
+							<li><a title="Nous soutenir" href="https://www.pluxml.org/static3/nous-soutenir">Nous soutenir</a></li>
+						</ul>
+					</div>
+					<div class="col sml-6 med-2">
+						<strong>Nous suivre ou nous contacter</strong>
+						<ul class="unstyled-list">
+							<li><a title="Nous contacter" href="https://www.pluxml.org/contact">Contact</a></li>
+							<li><a title="Twitter" href="https://twitter.com/pluxml">Twitter</a></li>
+							<li><a title="Google+" href="https://plus.google.com/u/0/communities/112244719352696146403">Google+</a></li>
+						<ul>
+					</div>
+					<div class="col sml-6 med-2">
+						<strong>En savoir plus</strong>
+						<ul class="unstyled-list">
+							<li><a title="Documentation" href="#">Documentation</a></li>
+							<li><a title="Forum" href="#">Forum</a></li>
+							<li><a title="Ressources" href="#">Ressources</a></li>
+						<ul>
+					</div>
+					<div class="col sml-6 med-3">
+						<strong>Autour de PluXml</strong>
+						<ul class="unstyled-list">
+							<li><a title="PluCSS" href="https://plucss.pluxml.org/">PluCSS</a></li>
+							<li><a title="Pluxopolis" href="http://pluxopolis.net/">Forum</a></li>
+							<li><a title="Visual Wizard" href="https://visualwizard.pluxml.org/">Visual Wizard</a></li>
+							<ul>
 					</div>
 				</div>
 			</div>
