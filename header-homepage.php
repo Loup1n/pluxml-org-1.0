a227f96a0ff36905d1094bf533a2b3ff01673419
P3ter <contact@p3ter.fr>
2018-11-10 13:40:06 +0100

    Isolement du menu de navigation

--- header-homepage.php
+++ header-homepage.php
@@ -35,28 +35,4 @@
 
 <body id="top" class="page mode-<?php $plxShow->mode(true) ?>">
 
-	<nav class="nav">
-			<div class="container">
-				<div class="grid">
-					<div class="col sml-12 med-3">
-						<ul class="menu text-center">
-							<li class="logo"><a href="https://pluxml.org" title="PluXml"><img class="float-left" src="<?php $plxShow->template(); ?>/img/favicon/favicon-32x32.png"><strong>PluXml.org</strong></a></li>
-						</ul>
-					</div>
-					<div class="col sml-12 med-9 lrg-6">
-						<ul class="menu text-center">
-							<li><a href="https://wiki.pluxml.org" title="Documentation">Documentation</a></li>
-							<li><a href="https://ressources.pluxml.org" title="Ressources">Ressources</a></li>
-							<li><a href="https://forum.pluxml.org" title="Forum">Forum</a></li>
-							<?php $plxShow->pageBlog('<li class="#page_class" id="#page_id"><a href="#page_url" title="#page_name">#page_name</a></li>'); ?>
-						</ul>
-					</div>
-					<div class="col sml-hide med-hide lrg-3 lrg-show">
-						<ul class="menu text-right">
-							<li><a href="http://plucss.pluxml.org/" title="PluCSS">PluCSS</a></li>
-							<li><a href="http://visualwizard.pluxml.org/" title="Visual Wizard">Visual Wizard</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</nav>
+  <?php include(dirname(__FILE__) . '/navigation.php'); ?>
