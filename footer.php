efd40734437f4c362be978396357054ddede355d
Pedro CADETE <contact@p3ter.fr>
2021-05-04 22:46:42 +0200

    Fix pluxml.org footer links

--- footer.php
+++ footer.php
@@ -11,16 +11,15 @@
 					<div class="col sml-6 med-2">
 						<strong>A propos de PluXml</strong>
 						<ul class="unstyled-list">
-							<li><a title="A propos" href="https://www.pluxml.org/a-propos-de-pluxml.html">A propos</a></li>
-							<li><a title="Nous soutenir" href="https://www.pluxml.org/nous-soutenir.html">Nous soutenir</a></li>
+							<li><a title="A propos" href="https://pluxml.org/a-propos-de-pluxml.html">A propos</a></li>
+							<li><a title="Nous soutenir" href="https://pluxml.org/static3/nous-soutenir">Nous soutenir</a></li>
 						</ul>
 					</div>
 					<div class="col sml-6 med-2">
 						<strong>Nous suivre ou nous contacter</strong>
 						<ul class="unstyled-list">
-							<li><a title="Nous contacter" href="https://www.pluxml.org/contact">Contact</a></li>
+							<li><a title="Nous contacter" href="https://pluxml.org/contact">Contact</a></li>
 							<li><a title="Twitter" href="https://twitter.com/pluxml">Twitter</a></li>
-							<li><a title="Google+" href="https://plus.google.com/u/0/communities/112244719352696146403">Google+</a></li>
 						<ul>
 					</div>
 					<div class="col sml-6 med-2">
@@ -47,7 +46,7 @@
 					<?php $plxShow->mainTitle('link'); ?> - <?php $plxShow->subTitle(); ?> &copy; GNU General Public License
 				</p>
 				<p>
-					<?php $plxShow->lang('POWERED_BY') ?>&nbsp;<a href="http://www.pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
+					<?php $plxShow->lang('POWERED_BY') ?>&nbsp;<a href="http://pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
 					<?php $plxShow->lang('IN') ?>&nbsp;<?php $plxShow->chrono(); ?>&nbsp;
 					<?php $plxShow->httpEncoding() ?>
 				</p>
